72f98998c934c7d4a57d651329a4d9007d5d0227
Philip Braun <philip.b@tickaroo.com>
2016-03-01 22:25:59 +0100

    quick fix

--- app/src/main/java/de/ur/mi/fashionapp/login/LoginPresenter.java
+++ app/src/main/java/de/ur/mi/fashionapp/login/LoginPresenter.java
@@ -1,5 +1,4 @@
 package de.ur.mi.fashionapp.login;
-
 import android.content.Context;
 import android.widget.Toast;
 import com.hannesdorfmann.mosby.mvp.MvpBasePresenter;
@@ -11,81 +10,79 @@ import com.parse.SignUpCallback;
 /**
  * Created by Philip on 24/02/2016.
  */
+// TODO: replace toasts with getView().showError(cause);
 public class LoginPresenter extends MvpBasePresenter<LoginView> {
   Context context;
 
-  public LoginPresenter(Context context) {
+  public LoginPresenter (Context context){
     this.context = context;
   }
 
+
   /*
-  *   Function for calling the login; needs the entries of the edit fields user/name and password
+  *   Function for calling the login; needs the entries of the edit fields username and password
    */
-  public void login(String mail, String p) {
-    if (mail.length() > 0) {
-      if (p.length() > 0) {
+  public void login(String username, String p) {
+    if(username.length()>0) {
+      if(p.length()>0) {
         // Call the Parse login method
-        ParseUser.logInInBackground(mail, p, new LogInCallback() {
-          @Override public void done(ParseUser user, ParseException e) {
-            if (e != null) {
-              Toast.makeText(context, (CharSequence) "Login hat nicht funktioniert.",
-                  Toast.LENGTH_SHORT).show();
-            } else {
-              Toast.makeText(context, (CharSequence) "Login hat geklappt.", Toast.LENGTH_SHORT)
-                  .show();
-              // TODO wichtig: asynchron, bei success:
+        ParseUser.logInInBackground(username, p, new LogInCallback() {
+          @Override
+          public void done(ParseUser user, ParseException e) {
+            if (e == null) {//if no error occurred
               if (isViewAttached()) {
                 getView().onLoginSuccess();
               }
+            } else {
+              Toast.makeText(context, (CharSequence) "Login didn't work", Toast.LENGTH_SHORT).show();
             }
           }
         });
-      } else {
-        Toast.makeText(context, (CharSequence) "Bitte Passwort eingeben", Toast.LENGTH_SHORT)
-            .show();
       }
-    } else {
-      Toast.makeText(context, (CharSequence) "Bitte Email-Adresse eingeben!", Toast.LENGTH_SHORT)
-          .show();
+      else {
+        Toast.makeText(context, (CharSequence) "No valid password", Toast.LENGTH_SHORT).show();
+      }
+    }
+    else{
+      Toast.makeText(context, (CharSequence) "No valid username", Toast.LENGTH_SHORT).show();
     }
   }
 
   /*
-  *   Function for calling the registering; needs the entries of the edit fields user/name, password and the confirmazation of the password
+  *   Function for calling the registering; needs the entries of the edit fields username, password and email adress
    */
-  public void register(String mail, String p, String p2) {
-    if (mail.length() > 0) {
-      if (!(p.length() > 0)) {
-        Toast.makeText(context, (CharSequence) "Passwort muss mindestens ein Zeichen enthalten",
-            Toast.LENGTH_SHORT).show();
-      } else if (p.equals(p2)) {
-        signup(mail, p);
-      } else {
-        Toast.makeText(context, (CharSequence) "Bitte zweimal das gleiche Passwort eingeben",
-            Toast.LENGTH_SHORT).show();
+  public void register(String username, String mail, String pw){
+    if(mail.length()>0) {
+      if(pw.length()>0){
+        if(username.length()>0) {
+          signup(username, mail, pw);
+        }
+        else
+          Toast.makeText(context, (CharSequence) "Username can't be empty", Toast.LENGTH_SHORT).show();
       }
-    } else {
-      Toast.makeText(context, (CharSequence) "Bitte Email-Adresse eingeben!", Toast.LENGTH_SHORT)
-          .show();
+      else
+        Toast.makeText(context, (CharSequence) "Password can't be empty", Toast.LENGTH_SHORT).show();
+    }
+    else{
+      Toast.makeText(context, (CharSequence) "E-mail can't be empty", Toast.LENGTH_SHORT).show();
     }
   }
 
   // Set up a new Parse user
-  private void signup(String mail, String p) {
+  private void signup(String username, String mail, String p){
     ParseUser user = new ParseUser();
-    user.setUsername(mail);
+    user.setEmail(mail);
+    user.setUsername(username);
     user.setPassword(p);
+
     user.signUpInBackground(new SignUpCallback() {
-      @Override public void done(ParseException e) {
-        if (e == null) {
-          Toast.makeText(context, (CharSequence) "Registriert", Toast.LENGTH_SHORT).show();
+      public void done(ParseException e) {
+        if (e == null) {//if no error occurred
           if (isViewAttached()) {
             getView().onRegisterSuccess();
           }
         } else {
-          if (isViewAttached()) {
-            getView().showError(e.getCause(), false);
-          }
+          Toast.makeText(context, (CharSequence) "Registering didn't work", Toast.LENGTH_SHORT).show();
         }
       }
     });
