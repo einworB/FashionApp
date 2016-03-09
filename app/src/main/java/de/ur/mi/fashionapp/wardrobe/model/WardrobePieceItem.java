e2ed89a2c3cac6290dafaf1a55010a99ea7bab64
Philip Braun <philip.b@tickaroo.com>
2016-03-09 15:42:10 +0100

    added dependencies; made wardrobe items serialisable

--- app/src/main/java/de/ur/mi/fashionapp/wardrobe/model/WardrobePieceItem.java
+++ app/src/main/java/de/ur/mi/fashionapp/wardrobe/model/WardrobePieceItem.java
@@ -1,7 +1,23 @@
 package de.ur.mi.fashionapp.wardrobe.model;
 
+import android.os.Parcel;
+import com.hannesdorfmann.parcelableplease.annotation.ParcelablePlease;
+
 /**
  * Created by Philip on 29/02/2016.
  */
-public class WardrobePieceItem extends WardrobeItem {
+@ParcelablePlease public class WardrobePieceItem extends WardrobeItem {
+
+  public static final Creator<WardrobePieceItem> CREATOR = new Creator<WardrobePieceItem>() {
+    @Override public WardrobePieceItem createFromParcel(Parcel source) {
+      WardrobePieceItem target = new WardrobePieceItem();
+      WardrobePieceItemParcelablePlease.readFromParcel(target, source);
+      return target;
+    }
+
+    @Override public WardrobePieceItem[] newArray(int size) {
+      return new WardrobePieceItem[size];
+    }
+  };
+
 }
