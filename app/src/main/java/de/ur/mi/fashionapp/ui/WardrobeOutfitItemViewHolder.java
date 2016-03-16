ded479a86e39ed424514fc860e7ae3036f62eeaf
magnimma <marioseebauer@web.de>
2016-03-16 09:54:10 +0100

    outfit, first part

--- app/src/main/java/de/ur/mi/fashionapp/ui/WardrobeOutfitItemViewHolder.java
+++ app/src/main/java/de/ur/mi/fashionapp/ui/WardrobeOutfitItemViewHolder.java
@@ -1,8 +1,13 @@
 package de.ur.mi.fashionapp.ui;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.support.percent.PercentRelativeLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+
 import de.ur.mi.fashionapp.R;
 import de.ur.mi.fashionapp.wardrobe.model.WardrobeItem;
 
@@ -12,11 +17,19 @@ import de.ur.mi.fashionapp.wardrobe.model.WardrobeItem;
 public class WardrobeOutfitItemViewHolder extends RecyclerView.ViewHolder {
 
   private PercentRelativeLayout outfitItem;
+  private ImageView main;
+  private ImageView left;
+  private ImageView mid;
+  private ImageView right;
 
   public WardrobeOutfitItemViewHolder(View itemView) {
     super(itemView);
     outfitItem = (PercentRelativeLayout) itemView.findViewById(R.id.outfitItem);
-    // TODO: show outfit title in outfit items??
+    // TODO: show outfit title in outfit items?? -> no
+    main = (ImageView)itemView.findViewById(R.id.mainPiece);
+    left = (ImageView)itemView.findViewById(R.id.left);
+    mid = (ImageView)itemView.findViewById(R.id.mid);
+    right = (ImageView)itemView.findViewById(R.id.right);
   }
 
   public void bind(final WardrobeItem item,
@@ -27,5 +40,11 @@ public class WardrobeOutfitItemViewHolder extends RecyclerView.ViewHolder {
       }
     });
     //TODO: get outfit / piece image(s)
+    /*
+    if(item.!=null) {
+      Bitmap bmp = BitmapFactory.decodeByteArray(item.getImage(), 0, item.getImage().length);
+      pic.setImageBitmap(bmp);
+    }
+    */
   }
 }
