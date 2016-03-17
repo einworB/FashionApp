c57940d4a7bb03df3012c894277b310cec0f7128
Philip Braun <philip.b@tickaroo.com>
2016-03-17 09:49:00 +0100

    grid in wardrobefragment

--- app/src/main/java/de/ur/mi/fashionapp/wardrobe/WardrobeFragment.java
+++ app/src/main/java/de/ur/mi/fashionapp/wardrobe/WardrobeFragment.java
@@ -3,7 +3,10 @@ package de.ur.mi.fashionapp.wardrobe;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +34,10 @@ public class WardrobeFragment extends
     View v = inflater.inflate(R.layout.fragment_wardrobe, container, false);
 
     return v;
+  }
+
+  @NonNull @Override protected RecyclerView.LayoutManager createRecyclerViewLayoutManager() {
+    return new GridLayoutManager(getContext(), 2, GridLayoutManager.VERTICAL, false);
   }
 
   @Override protected WardrobeAdapter createAdapter() {
