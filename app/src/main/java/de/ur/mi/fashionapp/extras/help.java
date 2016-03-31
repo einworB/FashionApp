b37f899b07f95937078a9155db76f3b2e019f2e3
Jana Helgath <jahe1601@googlemail.com>
2016-03-31 21:31:41 +0200

    added help

--- app/src/main/java/de/ur/mi/fashionapp/extras/help.java
+++ app/src/main/java/de/ur/mi/fashionapp/extras/help.java
@@ -1,17 +1,20 @@
 package de.ur.mi.fashionapp.extras;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import de.ur.mi.fashionapp.R;
 import de.ur.mi.fashionapp.wardrobe.WardrobeActivity;
 
 public class help extends AppCompatActivity {
+    private ImageView playVideoBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,17 @@ public class help extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        playVideoBtn = (ImageView) findViewById(R.id.play_video_button);
+        playVideoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = getResources().getString(R.string.help_video_url);
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
     }
 
     @Override
