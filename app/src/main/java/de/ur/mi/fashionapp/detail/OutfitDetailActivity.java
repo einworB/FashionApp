668e46e987ab5c11224ebce42549c9b1fad38df7
Jana Helgath <jahe1601@googlemail.com>
2016-03-31 16:43:56 +0200

    ui fixes

--- app/src/main/java/de/ur/mi/fashionapp/detail/OutfitDetailActivity.java
+++ app/src/main/java/de/ur/mi/fashionapp/detail/OutfitDetailActivity.java
@@ -31,7 +31,7 @@ import java.util.List;
 
 public class OutfitDetailActivity
         extends CBActivityMvpToolbar<RecyclerView, Object, DetailView, DetailPresenter>
-        implements DetailView, ImageViewholder.ImageViewholderClickListener {
+        implements DetailView {
 
     public static final String KEY_ITEM = "item";
     public static final String KEY_WARDROBE_ID = "wardrobe_id";
@@ -40,7 +40,6 @@ public class OutfitDetailActivity
 
     private WardrobeOutfitItem item;
     private ImageView mainPiece, pieceTwo, pieceThree, pieceFour, pieceFive, pieceSix, pieceSeven, pieceEight, pieceNine, pieceTen;
-    private OutfitDetailAdapter adapter;
     private int currentPosition;
     private int pieceCount;
     private List<Integer> loadedImagePositions = new ArrayList<>();
@@ -63,14 +62,9 @@ public class OutfitDetailActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //mainPiece = (ImageView) findViewById(R.id.mainPiece);
         setupImages();
         shareContainer = findViewById(R.id.shareContainer);
         sharingText = (TextView) findViewById(R.id.sharingText);
-        adapter = new OutfitDetailAdapter(this, this);
-        //contentView.setAdapter(adapter);
-        //contentView.setLayoutManager(
-                //new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -89,6 +83,15 @@ public class OutfitDetailActivity
 
     private void setupImages() {
         mainPiece = (ImageView) findViewById(R.id.imageView1);
+        mainPiece.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pieceID = item.getPieceIDs()[currentPosition];
+                startActivity(
+                        LinkService.getDetailIntent(OutfitDetailActivity.this, WardrobeFragment.TYPE_PIECE, null, wardrobeID,
+                                pieceID, getIntent().getExtras().getBoolean("isDetail")));
+            }
+        });
         pieceTwo = (ImageView) findViewById(R.id.imageView2);
         setOnClickListeners(pieceTwo, 1);
         pieceThree = (ImageView) findViewById(R.id.imageView3);
@@ -129,41 +132,6 @@ public class OutfitDetailActivity
         return new DetailPresenter(this, getApplicationContext());
     }
 
-  /* ORIGINAL METHOD!!!
-  private void setImage(String pieceID) {
-    int imageNumber = -1;
-    if (pieceID != null) {
-      String[] ids = item.getPieceIDs();
-      for (int i = 0; i < ids.length; i++) {
-        String id = ids[i];
-        if (pieceID.equals(id) && !loadedImagePositions.contains(i)) {
-          imageNumber = i;
-          loadedImagePositions.add(i);
-          break;
-        }
-      }
-    }
-    if (imageNumber != -1) {
-      // piececount is used to ensure only so many views are created as pieces exist in the oufit
-      List<Bitmap> bitmapList = new ArrayList<>(pieceCount);
-      Bitmap[] bitmaps = item.getImages();
-      for (int i = 0; i < pieceCount; i++) {
-        bitmapList.add(bitmaps[i]);
-      }
-      adapter.setItems(bitmapList);
-      //adapter.notifyItemChanged(imageNumber);
-      adapter.notifyDataSetChanged();
-      if (imageNumber == 0) {
-        Bitmap bitmap = item.getImages()[imageNumber];
-        if (bitmap != null) {
-          mainPiece.setImageBitmap(bitmap);
-          mainPiece.requestLayout();
-          currentPosition = 0;
-        }
-      }
-    }
-  }*/
-
     private void setImage(String pieceID) {
         int imageNumber = -1;
         if (pieceID != null) {
@@ -184,56 +152,16 @@ public class OutfitDetailActivity
             for (int i = 0; i < 10; i++) {
                 bitmapList.add(bitmaps[i]);
             }
-            adapter.setItems(bitmapList);
-            //adapter.notifyItemChanged(imageNumber);
-            adapter.notifyDataSetChanged();
-            /*if (imageNumber == 0) {
-                currentPosition = 0;
-            }*/
-            /*switch (imageNumber){
-                case 0: currentPosition = 0;
-                    break;
-                case 1: currentPosition = 1;
-                    break;
-                case 2: currentPosition = 2;
-                    break;
-                case 3: currentPosition = 3;
-                    break;
-                case 4: currentPosition = 4;
-                    break;
-                case 5: currentPosition = 5;
-                    break;
-                case 6: currentPosition = 6;
-                    break;
-                case 7: currentPosition = 7;
-                    break;
-                case 8: currentPosition = 8;
-                    break;
-                case 9: currentPosition = 9;
-                    break;
-                default:
-                    break;
-            }*/
             mainPiece.setImageBitmap(bitmapList.get(0));
-            mainPiece.requestLayout();
             pieceTwo.setImageBitmap(bitmapList.get(1));
-            pieceTwo.requestLayout();
             pieceThree.setImageBitmap(bitmapList.get(2));
-            pieceThree.requestLayout();
             pieceFour.setImageBitmap(bitmapList.get(3));
-            pieceFour.requestLayout();
             pieceFive.setImageBitmap(bitmapList.get(4));
-            pieceFive.requestLayout();
             pieceSix.setImageBitmap(bitmapList.get(5));
-            pieceSix.requestLayout();
             pieceSeven.setImageBitmap(bitmapList.get(6));
-            pieceSeven.requestLayout();
             pieceEight.setImageBitmap(bitmapList.get(7));
-            pieceEight.requestLayout();
             pieceNine.setImageBitmap(bitmapList.get(8));
-            pieceNine.requestLayout();
             pieceTen.setImageBitmap(bitmapList.get(9));
-            pieceTen.requestLayout();
         }
     }
 
@@ -285,7 +213,6 @@ public class OutfitDetailActivity
                 return true;
             case R.id.menu_outfit_detail_info:
                 String pieceID = this.item.getPieceIDs()[currentPosition];
-                Log.d("TTT", "currPos: " + currentPosition + " id: " + pieceID);
                 startActivity(
                         LinkService.getDetailIntent(this, WardrobeFragment.TYPE_PIECE, null, wardrobeID,
                                 pieceID, getIntent().getExtras().getBoolean("isDetail")));
@@ -295,28 +222,19 @@ public class OutfitDetailActivity
         }
     }
 
-    @Override
-    public void onImageClicked(int position) {
-        Bitmap bitmap = item.getImages()[position];
-        mainPiece.setImageBitmap(bitmap);
-        mainPiece.requestLayout();
-        currentPosition = position;
-    }
-
     private void switchImages(View v, int position){
         Bitmap list[] = item.getImages();
-        //Bitmap bitmap = item.getImages()[position];
-        Bitmap bitmap = list[position];
-        //Bitmap temp = item.getImage();
-        Bitmap temp = list[0];
-        mainPiece.setImageBitmap(bitmap);
-        mainPiece.requestLayout();
-        ((ImageView) v).setImageBitmap(temp);
-        //item.setImage(bitmap);
-        list[0] = bitmap;
-        list[position] = temp;
-        item.setImages(list);
-        currentPosition = position;
+        if(list[position]!=null){
+            Bitmap bitmap = list[position];
+            Bitmap temp = list[0];
+            mainPiece.setImageBitmap(bitmap);
+            mainPiece.requestLayout();
+            ((ImageView) v).setImageBitmap(temp);
+            list[0] = bitmap;
+            list[position] = temp;
+            item.setImages(list);
+            currentPosition = position;
+        }
     }
 
     @Override
