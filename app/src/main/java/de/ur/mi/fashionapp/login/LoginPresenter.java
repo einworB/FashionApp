0443545c5ae2798927cdd97cf2ce9ac802824d28
magnimma <marioseebauer@web.de>
2016-03-31 16:47:49 +0200

    register bug fixed

--- app/src/main/java/de/ur/mi/fashionapp/login/LoginPresenter.java
+++ app/src/main/java/de/ur/mi/fashionapp/login/LoginPresenter.java
@@ -2,6 +2,7 @@ package de.ur.mi.fashionapp.login;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.util.Log;
 import android.widget.Toast;
 import com.hannesdorfmann.mosby.mvp.MvpBasePresenter;
 import com.parse.LogInCallback;
@@ -72,50 +73,60 @@ public class LoginPresenter extends MvpBasePresenter<LoginView> {
     }
   }
 
-  // Set up a new Parse user
-  private void signup(String username, String mail, String p){
-    ParseUser user = new ParseUser();
-    user.setEmail(mail);
-    user.setUsername(username);
-    user.setPassword(p);
-    getView().showLoading(false);
-    user.signUpInBackground(new SignUpCallback() {
-      public void done(ParseException e) {
-        if (e == null) {//if no error occurred
-          if (isViewAttached()) {
-            addFirstWardrobe();
-            getView().onRegisterSuccess();
-            getView().showContent();
-          }
-        } else {
-          if(e.getCode()==ParseException.CONNECTION_FAILED){
-            Toast.makeText(context, "No internet connection", Toast.LENGTH_LONG).show();}
-          else if(isViewAttached()) getView().showError(e, false);
-        }
-      }
-    });
-  }
+    // Set up a new Parse user
+    private void signup(String username, String mail, String p) {
+        ParseUser.logOut();
+        ParseUser user = new ParseUser();
+        user.setEmail(mail);
+        user.setUsername(username);
+        user.setPassword(p);
+        getView().showLoading(false);
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {//if no error occurred
+                    if (isViewAttached()) {
+                        addFirstWardrobe();
 
-  public void addFirstWardrobe(){
-    String wardropeName = "My first Wardrobe";
+                    }
+                } else {
+                    if (e.getCode() == ParseException.CONNECTION_FAILED) {
+                        Toast.makeText(context, "No internet connection", Toast.LENGTH_LONG).show();
+                    } else if (isViewAttached()) getView().showError(e, false);
+                }
+            }
+        });
+    }
 
-    //This Method is to create a new wardrope;
-    String userID = ParseUser.getCurrentUser().getObjectId();
-    ParseObject wr = new ParseObject("Wardrope");
-    wr.put("Name", wardropeName);
-    wr.put("UserID", userID);
-    getView().showLoading(true);
-    wr.saveInBackground(new SaveCallback() {
-      @Override
-      public void done(com.parse.ParseException e) {
-        if (e != null) {
-          if(e.getCode()==ParseException.CONNECTION_FAILED){
-            Toast.makeText(context, "No internet connection", Toast.LENGTH_LONG).show();}
-          else if(isViewAttached()) getView().showError(e, false);
-        }
-      }
-    });
-  }
+    public void addFirstWardrobe() {
+        String wardropeName = "My first Wardrobe";
+        String userID = ParseUser.getCurrentUser().getObjectId();
+        final ParseObject wr = new ParseObject("Wardrope");
+        wr.put("Name", wardropeName);
+        wr.put("UserID", userID);
+        if (isViewAttached()) getView().showLoading(true);
+        wr.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(com.parse.ParseException e) {
+                if (e == null) {
+                    ParseUser user = ParseUser.getCurrentUser();
+                    user.put("currentWardrobeID", wr.getObjectId());
+                    user.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (isViewAttached()&&e==null) {
+                                getView().onRegisterSuccess();
+                                getView().showContent();}
+                        }
+                    });
+
+                } else {
+                    if (e.getCode() == ParseException.CONNECTION_FAILED) {
+                        Toast.makeText(context, "No internet connection", Toast.LENGTH_LONG).show();
+                    } else if (isViewAttached()) getView().showError(e, false);
+                }
+            }
+        });
+    }
 
   public void resetPassword(final String userName) {
     AlertDialog.Builder builder = new AlertDialog.Builder(context);
